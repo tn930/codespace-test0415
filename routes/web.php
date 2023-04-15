63acd6e300b4161dbc2c245fc448cb6116bf72c0
clutch-t-mitani <85106049+clutch-t-mitani@users.noreply.github.com>
2023-04-15 05:14:13 +0000

    codespace first

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TestController::class, 'index'])->name('index');
